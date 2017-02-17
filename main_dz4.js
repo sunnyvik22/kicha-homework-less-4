79dbe2744832570a440efdaa7a7e97ffd44425c8
sunnyvik22 <sunny_vik22@mail.ru>
2017-02-17 21:26:44 +0200

    new file losson4

--- main_dz4.js
+++ main_dz4.js
@@ -18,7 +18,9 @@
  console.table(matrix);
   */
 
-function rand(startNum, endNum) {
+  //домашка, dz1
+
+/*function rand(startNum, endNum) {
   return Math.floor(startNum + Math.random() * ((endNum + 1) - startNum));
 }
 var ARR_SIZE2=10;
@@ -62,3 +64,59 @@ console.log(leftSide);
 
 var resaltArr=topSide.concat(rightSide,bottomSide,leftSide);
 console.log(resaltArr);
+*/
+
+/*http://license-server.tk:1017/*/
+
+//домашка, dz3
+
+function rand(startNum, endNum) {
+  return Math.floor(startNum + Math.random() * ((endNum + 1) - startNum));
+}
+
+var startNum=0;
+var endNum =100;
+
+var arrDz = [];
+    arrDz.length=10;
+  for (var i =0; i<arrDz.length; i++) {
+     arrDz[i]=[];
+       for (var y=0; y<arrDz.length; y++) {
+         var num = rand(startNum,endNum);   
+          arrDz[i].push(num);
+       }  
+  }
+console.table (arrDz);
+
+var topSide =arrDz[0].slice();
+console.log(topSide);
+
+var rightSide =[];
+var lastIndex=arrDz.length-1;
+  for (var i = 1; i < arrDz.length; i++) {
+       rightSide.push(arrDz[i][lastIndex]);
+  }
+
+  console.log(rightSide);
+
+var diagonal=[];
+
+ /*for (var i = (arrDz.length-2); i >=1 ; i--) {   // прикольно получилось, сама не ожидала
+         for (y=1; y <(arrDz.length-2); y++) {     // надо оставить )
+
+        diagonal.push(arrDz[i][lastIndex-y]); 
+
+       }
+       
+ }
+ console.log(diagonal);*/
+
+  var count=1;
+  for (var i = (arrDz.length-2); i >=1 ; i--) {  
+        diagonal.push(arrDz[i][lastIndex-count]); 
+        count++;  
+ }
+ console.log(diagonal);
+
+ var resaltArrDz=topSide.concat(rightSide,diagonal);
+ console.log(resaltArrDz);
